5f5cea4bc69eef28cb17fd81dc4744aa847667e1
Morten Eidet <morteneid@uia.no>
2023-10-16 14:15:59 +0200

    Added a signup screen, missing db

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ app = Flask(__name__)
 # A simple dictionary that stores username and password
 users = {'john': 'doe'}
 
+
 # Home route
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 # Sign in route
 @app.route('/signin', methods=['GET', 'POST'])
@@ -22,10 +24,22 @@ def signin():
             return render_template('signin.html', message='Invalid credentials. Please try again.')
     return render_template('signin.html')
 
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        users[username] = password
+        return redirect(url_for('signin'))
+    return render_template('signup.html')
+
+
 # Dashboard route
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
