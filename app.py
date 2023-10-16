e2c9137ccd172ece727af8f0c86fdf9e4316f292
Morten Eidet <morteneid@uia.no>
2023-10-16 12:08:49 +0200

    updated so there is a basic login

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# A simple dictionary that stores username and password
+users = {'john': 'doe'}
 
+# Home route
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    return render_template('home.html')
 
+# Sign in route
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users and users[username] == password:
+            return redirect(url_for('dashboard'))
+        else:
+            return render_template('signin.html', message='Invalid credentials. Please try again.')
+    return render_template('signin.html')
+
+# Dashboard route
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
